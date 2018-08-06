e47a9fb894818de3892d00bb2cddcea1c21e9aa9
Michael Nino Evensen <michaelevensen@gmail.com>
2018-08-06 21:25:41 +0200

    removed empty controller.

--- UXARImageTracking/AudioController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AudioController.swift
-//  UXARImageTracking
-//
-//  Created by Michael Nino Evensen on 06/08/2018.
-//  Copyright © 2018 Michael Nino Evensen. All rights reserved.
-//
-
-import Foundation
